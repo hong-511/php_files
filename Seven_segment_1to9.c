639035c18acc0ea1fe0b97f31c12c12758a20f82
hong-511 <103887175+hong-511@users.noreply.github.com>
2023-02-24 00:55:04 +0800

    Add files via upload

---
+++ Seven_segment_1to9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <reg52.h>
+
+#define LED7 P0
+code unsigned char font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+
+void delay(unsigned int i)
+{
+	unsigned int j, k;
+	for(j = 0; j < i; j++)
+		for (k = 0; k < 100; k++);
+}
+
+void main()
+{
+	unsigned int i = 0;
+	while(1)
+	{
+		LED7 = font[i++];
+		delay(500); 
+		if (i == 10)
+			i = 0;
+	}
+ }
---
+++ counter.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <reg52.h>
+
+#define BUS P0
+
+sbit font_ct1 = P2^0;
+sbit scan_ct1 = P2^1;
+
+code unsigned char font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+code unsigned char scan[] = {0xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf, 0x7f};
+
+void delay(unsigned int i)
+{
+	unsigned int j, k;
+	for(j = 0; j < i; j++)
+		for (k = 0; k < 10; k++);
+}
+
+void put_font(unsigned char i)
+{
+	BUS = 0xff - font[i];
+	font_ct1 = 1;
+	font_ct1 = 0;
+}
+
+void put_digital(unsigned char i)
+{
+	BUS = scan[i];
+	scan_ct1 = 1;
+	scan_ct1 = 0;
+}
+
+void display(unsigned long temp)
+{
+	unsigned int i = 0;
+	for(i = 0; temp; i++)
+	{
+		put_font(temp % 10);
+		put_digital(7 - i);
+		delay(5);
+		temp /= 10;
+	}
+}
+
+void main()
+{
+	unsigned int i = 0;
+	unsigned int p = 0;
+	unsigned long cnt = 0;
+	cnt = 0;
+	while (1)
+	{
+		if(p++ == 10)
+		{
+			p = 0;
+			cnt++;
+		}
+		display(cnt);
+	}
+ }
