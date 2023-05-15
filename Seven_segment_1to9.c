8642d9d7f3108ff3a145251cbfe24b50349b54b7
hong-511 <103887175+hong-511@users.noreply.github.com>
2023-05-15 10:30:08 +0800

    Update Seven_segment_1to9.c

--- Seven_segment_1to9.c
+++ Seven_segment_1to9.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <reg52.h>
-
+https://docs.google.com/presentation/d/1fgwxs5Yn5LeqnWZrwyqrnIdQtZReIFTVthRgy-yrbkM/edit?usp=sharing
 #define LED7 P0
 code unsigned char font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
